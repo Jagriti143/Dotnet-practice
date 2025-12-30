714d83452e4b259d609c133a3973222b7d82d546
jagriti <119938056+Jagriti143@users.noreply.github.com>
2025-12-30 14:28:53 +0530

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,160 @@
+﻿// using System;
+// using System.Linq;
+// using System.Collections;
+// using System.Collections.Generic;
+// class Program{
+//     public static void Main(string[] args){
+
+//         // string x=Console.ReadLine();
+//         //    if(x.length<6 ){
+                
+//         //    }
+          
+
+
+
+//         // int[] arr={1,2,3,2,1,4,2};
+//         // Dictionary<int,int> dict=new Dictionary<int,int>();
+//         // foreach(int k in arr){
+//         //     if(!dict.ContainsKey(k) ){
+//         //     dict.Add(k,1);
+//         //     }else{
+//         //         dict[k]++;
+//         //     }
+//         // }
+//         // foreach(var k in dict){
+//         //     Console.WriteLine($"Key: {k.Key} , Value:{k.Value}");
+//         // }
+
+
+//         // int[] arr1={1,2,3};
+//         // int[] arr2={4,5,6};
+//         // List<int> l=new List<int>();
+//         // int m=arr1.Length;
+//         // int n=arr2.Length;
+//         // int i=0;
+//         // int j=0;
+//         // while(i<m && j<n){
+//         //     if(arr1[i]<arr2[j]){
+//         //         l.Add(arr1[i]);
+//         //         i++;
+//         //     }else{
+//         //         l.Add(arr2[j]);
+//         //         j++;
+//         //     }
+//         // }
+//         // while(i<m){
+//         //     l.Add(arr1[i]);
+//         //     i++;
+//         // }
+//         // while(j<n){
+//         //     l.Add(arr2[j]);
+//         //     j++;
+//         // }
+//         // foreach(int k in l){
+//         //     Console.Write(k+" ");
+//         // }
+
+
+//     // int[] arr={1,2,3};
+//     // foreach(int i in arr){
+//     //     Console.WriteLine(i);
+//     // }
+//     //int[] marks={10,20,30};
+//     // Array.Clear(marks,1,marks.Length-1);
+//     // foreach(int i in marks){
+//     // Console.WriteLine(i);
+//     // }
+//     // int[] src={10,20,30};
+//     // int[] dest=new int[3];
+//     // Array.Copy(src,dest,2);
+//     // for(int i=0;i<3;i++){
+//     //     Console.Write(dest[i]+" ");
+//     // }
+
+//     //int[] nums={1,2};
+
+//     // Array.Resize(ref nums,4);
+//     // for(int i=0;i<4;i++){
+//     //     Console.Write(nums[i]+" ");
+//     // }
+
+
+//     // Array.Resize(nums,4);  //nums would be passed by value
+//     //Only a copy of the reference is sent
+//     // Changes would be lost outside the method
+//     // for(int i=0;i<4;i++){
+//     //     Console.Write(nums[i]+" ");
+//     // }
+//     //int[] nums2={3,4,7,8,9};
+//     // Array.Resize( ref nums2,1);
+//     // for(int i=0;i<1;i++){
+//     //     Console.WriteLine(nums2[i]);
+//     // }
+
+//     // bool found=Array.Exists(nums2,x=>x>4);
+//     // Console.WriteLine(found);
+
+//     // List<int> list=new List<int>();  
+//     // list.Add(1);
+//     // list.Add(2);
+//     // list.Add(3);
+//     // foreach(int i in list){
+//     // Console.WriteLine(i);
+//     // }
+
+//    // Hashtable hs=new Hashtable();
+// //    Dictionary<int,string> hs = new Dictionary<int,string>();
+// //     hs.Add(1,"Admin");
+// //     hs.Add(2,"Jagriti");
+// //     hs.Add(3,"user");
+// //    // hs.Add(3,"Jagriti");
+// //     //Console.WriteLine(hs[1]);
+// //     foreach (var i in hs)
+// //     {
+// //     Console.WriteLine($"Key={i.Key} and Value={i.Value}");
+// //     }
+
+// //     SortedList <string, string> list = new SortedList <string, string>();
+// //     list.Add("Jagriti", "B") ;
+// //     list.Add("Karan","A");
+// //    for (int i = list.Count - 1; i >= 0; i--)
+// //         {
+// //             Console.WriteLine($"Key={list.Keys[i]}, Value={list.Values[i]}");
+// //         }
+//     // {
+//     // Console.WriteLine($"Key={i.Key} and Value={i.Value}");
+//     // }
+
+
+//     // foreach (var i in hs)
+//     //     {
+//     //         Console.WriteLine($"Key={i.Key} and  Value={i.Value}");
+//     //     }
+
+
+
+  
+
+
+//     // int[,] matrix=new int[2,3]{
+//     //     {1,2,3},{4,5,6}
+//     // };
+//     // for(int i=0;i<matrix.GetLength(0);i++){
+//     //     for(int j=0;j<matrix.GetLength(1);j++){
+//     //         Console.Write(matrix[i,j]+" ");
+//     //     }
+//     //     Console.WriteLine();
+//     // }
+//     // int[][] jagged=new int[2][];
+//     // jagged[0]=new int[] {1,2};
+//     // jagged[1]=new int[]{3,4,5};
+//     // for(int i=0;i<jagged.Length;i++){
+//     //     for(int j=0;j<jagged[i].Length;j++){
+//     //         Console.Write(jagged[i][j]+" ");
+//     //     }
+//     //     Console.WriteLine();
+//     // }
+//     }
+// }
+
