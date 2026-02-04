377cee2b67b2b7a08705a1105ac0eeb2c4130c49
jagriti <119938056+Jagriti143@users.noreply.github.com>
2026-02-04 20:55:47 +0530

    Add files via upload

---
+++ TopBrains/Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> ls=new List<int>{1,4,5};
+        Dictionary<int,int> dict=new Dictionary<int, int>{{1,20000},{4,40000},{5,15000}};
+            int total=0;
+            foreach(int i in ls)
+        {
+            if(dict.ContainsKey(i)){
+            total+=dict[i];
+            }
+            
+        }
+        Console.WriteLine(total);
+            
+        }
+}
+
