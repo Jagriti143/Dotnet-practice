48b1cd5f7cc8172a65f792efb85f55a6e7da63c7
jagriti <119938056+Jagriti143@users.noreply.github.com>
2026-02-04 21:03:21 +0530

    Add files via upload

---
+++ TopBrains/LargestThree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Program
+{
+    public static int Largest(int a,int b,int c)
+    {
+        if(a>b && a > c)
+        {
+            return a;
+        }else if(b>a && b > c)
+        {
+            return b;
+        }else{
+            return c;
+        }
+        return 0;
+
+    }
+    public static void Main(string[] args)
+    {
+        int l=Largest(9,4,2);
+        Console.WriteLine(l);
+    }
+}
