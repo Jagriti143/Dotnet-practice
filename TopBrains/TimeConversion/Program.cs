d9771f57c933b7eb8d80d2e19be2db391550e111
jagriti <119938056+Jagriti143@users.noreply.github.com>
2026-02-04 21:24:15 +0530

    Add files via upload

---
+++ TopBrains/TimeConversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sec=int.Parse(Console.ReadLine());
+        double ot=(double)sec/60;
+        StringBuilder s=new StringBuilder();
+        s.Append(ot.ToString("F2"));
+        s.Replace(".",":");
+        Console.WriteLine(s);
+
+    }
+}
