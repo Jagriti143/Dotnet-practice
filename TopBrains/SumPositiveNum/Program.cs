5db2f1d9b96fbffc32b1815eab87cadcaff7fb66
jagriti <119938056+Jagriti143@users.noreply.github.com>
2026-02-04 21:43:27 +0530

    Add files via upload

---
+++ TopBrains/SumPositiveNum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n=6;
+        int[] arr=new int[n];
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i]=int.Parse(Console.ReadLine());
+        }
+        int sum=0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            
+            if (arr[i] == 0)
+                break;
+            
+            if (arr[i] < 0)
+                continue;
+            
+            sum+=arr[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
