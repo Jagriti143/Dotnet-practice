ed97e6627139a8968fb203e9ac12fc058a68268f
jagriti <119938056+Jagriti143@users.noreply.github.com>
2026-02-07 15:49:11 +0530

    Add files via upload

---
+++ TopBrains/MidPointRounding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static double Area(double radius)
+    {
+        double pii=Math.PI;
+        double area=pii*radius*radius;
+        return area;
+    }
+    public static void Main(string[] args)
+    {
+        double radius=double.Parse(Console.ReadLine());
+        double area=Math.Round(Area(radius),2,MidpointRounding.AwayFromZero);
+        Console.WriteLine(area);
+    }
+}
